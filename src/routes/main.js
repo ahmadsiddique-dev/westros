4918b66b3dba30b1851099dbbe56dd0c48fbcbea
Ahmad Siddique <asshikrani66@gmail.com>
2026-03-25 20:38:07 +0500

    ah I don't know

--- src/routes/main.js
+++ src/routes/main.js
@@ -5,5 +5,7 @@ import { Authrouter } from "./auth.route.js";
 const router = Router();
 
 router.use('/auth', Authrouter);
+router.use('/got/:house', GOTRouter)
+router.use('/hotd/:house', HOTDRouter)
 
-export default router;
+export default router;
